2bce8508f873ae812dfa93458e570beb8c3c03e9
mrtheidiot <mrtheitiot@gmail.com>
2022-02-22 16:14:06 +0100

    changes

--- src/components/Events/Entollment.js
+++ src/components/Events/Entollment.js
@@ -1,10 +1,22 @@
-import React from 'react'
-import './Enrollment.css';
+import React from "react";
+import "./Enrollment.css";
 
-const Entollment = () => {
+const Entollment = (props) => {
   return (
-    <div>Entollment</div>
-  )
-}
+    <>
+      <div className="entollment__fixed" onClick={props.callback}>""</div>
+      <div className="enrollment__absolute" onClick={null}>
+        <div className="enrollment__text">
+          Zapisy na wybrany kurs następują poprzez email. W wiadomości proszę
+          podać wybrany kurs oraz date.
+          <br></br>
+          <br></br>
+          <br></br>
+          {props.enrollment}
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Entollment
+export default Entollment;
