f049bc521c20db6c99e49ee9d075c73e26ade363
Piotr Ginda <mrtheidiot@gmail.com>
2022-05-10 15:22:49 +0200

    changes

--- src/components/Poducts/Sukienki/Sukienki1.js
+++ src/components/Poducts/Sukienki/Sukienki1.js
@@ -1,35 +1,43 @@
 import React, { useState, useEffect } from "react";
 import classes from "./Sukienki1.module.css";
-import zdj1 from "./../../../assets/p1.jpg";
+import zdj1 from "./../../../assets/spodniczka1.jpg";
+import './Sukienki1.css';
 
 const Sukienki1 = () => {
   const itemlist = [
     {
-      nazwa: "Spodniczka standardowa",
+      nazwa: "Spodniczki standardowe",
       link: "/kontakt",
-      description: ["taka i taka spodniczka", "taka i taka"],
+      description1: ["Gładka, rozkloszowania spódniczka taneczna. Model z szerokim pasem okalającym biodra oraz falbaną z pełnego koła."],
+      description2: ["W pasie spódnicę utrzymuje gumka. Materiał jest lejący, dzięki czemu spódnica ładnie się układa."],
       img: `${zdj1}`,
+      direction: 0,
+
     },
     {
-      nazwa: "Spodniczka standardowa",
+      nazwa: "Spodniczki dla wysokich psów",
       link: "/kontakt",
-      description: ["taka i taka spodniczka", "taka i taka"],
+      description1: ["Gładka, rozkloszowania spódniczka taneczna. Model z szerokim pasem okalającym biodra oraz falbaną z pełnego koła."],
+      description2: ["W pasie spódnicę utrzymuje gumka. Materiał jest lejący, dzięki czemu spódnica ładnie się układa."],
       img: `${zdj1}`,
+      direction: 1,
     },
   ];
+
 
   return (
     <>
       <div className={classes.wrapper}>
         {itemlist.map((item) => (
-          <div className={classes.section}>
+          <div className={item.direction === 0 ? 'sukienki_right' : 'sukienki_left'}>
             <div className={classes.image}>
-              <img src={item.img}
-
-
-
-
-          
+              <img src={item.img} alt="piesek" />
+            </div>
+            <div className={classes.textarea}>
+              <div className={classes.title}>{item.nazwa}</div>
+              <div className={classes.opis}>{item.description1}</div>
+              <div className={classes.opis}>{item.description2}</div>
+            </div>
           </div>
         ))}
       </div>
